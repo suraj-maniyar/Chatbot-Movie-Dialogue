0762249eac7613fd9aea95e878e49c23d4fe75cd
suraj-maniyar <spmaniyar7@gmail.com>
2018-11-17 00:55:35 -0500

    Updated Code(Comments)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 import os
 
 
 os.chdir('..')
 
+############################################################################################################
 
+# Threshold maximum number of words to be used in a dialogue. Dialogs having more number of words than this threshold are simply discarded.
+max_seq_length = 100
+
+# Dimension of word vector
+dimension = 50
+
+# Total number of conversations which we consider for training.
+total_convs = 200   # len(convs)
+
+
+############################################################################################################
+
+# Returns dictionary which gives vectors corresponding to words
 def loadGloveModel(gloveFile):
     print("Loading Glove Model")
     f = open(gloveFile,'r')
@@ -22,6 +37,19 @@ def loadGloveModel(gloveFile):
 def preprocess(text):
     text = text.lower()
     text = text.replace("you're", "you are")
+    text = text.replace("he's", "he is")
+    text = text.replace("she's", "she is")
+    text = text.replace("it's", "it is")
+    text = text.replace("what's", "what is")
+    text = text.replace("that's", "that is")
+    text = text.replace("there's", "there is")
+    text = text.replace("how's", "how is")
+    text = text.replace("'ll", " will")
+    text = text.replace("'ve", " have")
+    text = text.replace("won't", "will not")
+    text = text.replace("'d", " would")
+    text = text.replace("'can't", "can not")
+    text = text.replace("i'm", "i am")
     text = text.replace('.', ' <eos> ')
     text = text.replace(",", "")
     text = text.replace(";", "")
@@ -46,7 +74,6 @@ def preprocess(text):
     text = text.replace('[', ' ')
     text = text.replace(']', ' ')
     text = text.replace('/', ' ')
-
     return text
 
 
@@ -64,10 +91,11 @@ def getVector(lineNo, glove_model, max_seq_length, vocab):
             vect.append( glove_model[sentence[i]] )
             #print(sentence[i])
         else:
-            print('UNK')
+            #print('UNK')
             vect.append( glove_model['<unk>'] )
     vect = np.array(vect)
     return vect
+
 
 # Returns nearest word to the given vector
 def getWord(vec, glove_model):
@@ -106,45 +134,49 @@ for line in conv_lines:
   convs.append(_line.split(','))
 
 
-# List storing maximum no. of words used in a dialogue for all conversations.
+'''
+# List storing maximum no. of words used in all dialogs in all conversations. Use this to set 'Maximum Sequence Length'
 len_arr = []
-
 for conv_index in range(len(convs)):
     # Check if the conversation is valid. i.e. no. of dialogues in conversation is greater than 1
     if(len(convs[conv_index]) > 1):
        for i in range(len(convs[conv_index])):
          len_arr.append( len(id2line[convs[conv_index][i]].split()) )
 len_arr.sort(reverse=True)
+'''
 
-# Threshold maximum number of words to be used in a dialogue.
-max_seq_length = 100
 
+
+# Contains line numbers for which sentences are longer than threshold length
 lines_to_ignore = []
 for line_ids in list(id2line.keys()):
     if( len( id2line[line_ids].split() ) > max_seq_length ):
         lines_to_ignore.append(line_ids)
 
 
+print('Generating vocab....')
+# List to store all valid dialogues. (Text Corpus)
 text_arr = []
-# If any conversation has a dialogue which appears in dialogs_to_ignore, we discard that conversation all together
+# If any conversation has a dialogue which appears in lines_to_ignore, we discard that conversation all together
 for conv_index in range(len(convs)):
-    # The intersecrion returns set of elements which are present in both the lists
+    # lines from conversation which do not contain a single ignore word.
     if(  set(convs[conv_index]) & set(lines_to_ignore)  ==  set() ):
         for i in range(len(convs[conv_index])-1):
             text_arr.append( id2line[ convs[conv_index][i]   ] )
 
 
 
-print('max_seq_length', max_seq_length)
-print('lines_to_ignore', len(lines_to_ignore))
-print('len(text_arr) : ', len(text_arr))
+#print('max_seq_length : ', max_seq_length)
+print('Total number of lines to ignore : ', len(lines_to_ignore))
+print('Total number of sentences : ', len(text_arr))
 
-
+# Union of words in text corpus
 words_list2 = [element.split() for element in text_arr]
 word_list = []
 for sublist in words_list2:
     for item in sublist:
         word_list.append(item)
+
 
 # Vocab list pertaining to our data (Cornell Movie Data)
 vocab = list(set(word_list))
@@ -155,34 +187,36 @@ print('Vocab Size = ', len(vocab))
 
 glove_model = loadGloveModel('/home/suraj/Dataset/glove.6B/glove.6B.50d.txt')
 glove_model['<eos>'] = glove_model['.']
-glove_model['<pad>'] = np.zeros(50)
+glove_model['<pad>'] = np.zeros(dimension)
 glove_list = list(glove_model.keys())
 
+# Words which are in vocab and have a vector representation.
 intersection =  set(vocab) & set(glove_list)
+
+# List containing words which do not have any vector representation.
 no_mapping = list(set(vocab) - intersection)
-print('No Mapping : ', len(no_mapping))
+print('Number of words from vocab with no mapping : ', len(no_mapping))
 
-
+# Removing those words from our original vocabulary, which do not have a vector representation
 vocab = list( set(vocab) - set(no_mapping) )
 print('New Vocab Size : ', len(vocab))
 
+# Converting list of vocabulary to a dictionary of vocabulary. word2vec is same as glove_model, but with only those
+# keys which are present in our corpus. This helps reduce size of our dictionary and making vector2word computation more efficient.
 word2vec = {}
-
 for key in vocab:
     word2vec[key] = glove_model[key]
-
 print('word2vec size : ', len(word2vec.keys()))
 
 
 X = []
 Y = []
 
-'''
-total_convs = 60   # len(convs)
 
+print('Generating X and Y arrays')
 for conv_index in range(total_convs):
     # The intersecrion returns set of elements which are present in both the lists
-    print(conv_index, total_convs)
+    print('\nConversation',conv_index, '/', total_convs, ' Sentences:',len(convs[conv_index]))
     if(  set(convs[conv_index]) & set(lines_to_ignore)  ==  set() ):
         for  i in range(len(convs[conv_index])-1):
             vectorX = getVector(convs[conv_index][i], word2vec, max_seq_length, vocab)
@@ -190,9 +224,20 @@ for conv_index in range(total_convs):
             X.append(vectorX)
             Y.append(vectorY)
 
+print('Converting to numpy array...')
 X = np.array(X)
 Y = np.array(Y)
 
 print(X.shape)
 print(Y.shape)
-'''
+
+print('Shuffling...')
+X_train, X_CV, Y_train, Y_CV = train_test_split(X, Y, test_size=0.2, random_state=7)
+
+print('Train')
+print(X_train.shape)
+print(Y_train.shape)
+
+print('\nCV')
+print(X_CV.shape)
+print(Y_CV.shape)
